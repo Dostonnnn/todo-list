fab5db43f698a3e76d1ba94923de3c7f596d829d
Dostonnnn <davronovvv707@gmail.com>
2026-04-07 19:05:55 +0500

    done

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
 const elForm = document.querySelector(".form");
+const elMes = document.querySelector(".mes");
 const elInput = document.querySelector(".input");
-const elMessage = document.querySelector(".mes");
-const elUl = document.querySelector("ul");
+const elLists = document.querySelector(".lists");
 let array = [];
 elForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  const elValue = elInput.value.trim();
-  if (elValue.length > 20) {
-    elMessage.textContent = "20 char dan oshmasin!";
+  const value = elInput.value.trim().toLocaleLowerCase();
+  if (value.length > 20) {
+    elMes.textContent = "Please enter less than 20 characters!";
     elForm.reset();
+  } else if (value.length <= 0) {
+    elMes.textContent = "Please fill the blank!";
   } else {
-    if (!elValue)
-      return (elMessage.textContent = "Iltimos maydonni toldiring!");
-    else {
-      elMessage.textContent = "";
-      const info = {
-        value: elValue,
-        date: "12.02.2026",
-        isCompleted: false,
-      };
-      array.unshift(info);
-      showInformation(array);
-      elForm.reset();
-    }
+    elMes.textContent = "";
+    const object = {
+      id: Date.now(),
+      name: value,
+      year: "13",
+      janr: "salom",
+    };
+    array.unshift(object);
+    elForm.reset();
+    showUI(array);
   }
 });
-function showInformation(infor) {
-  elUl.innerHTML = "";
-  infor.forEach((element) => {
-    elUl.innerHTML += `
+
+function showUI(array) {
+  elLists.innerHTML = "";
+  array.forEach((element) => {
+    elLists.innerHTML += `
           <li>
-            <h5>${element.value}</h5>
-            <p>${element.date}</p>
+            <h5>${element.name}</h5>
+            <p>${element.year}</p>
+            <p>${element.janr}</p>
             <button class="edit">Edit</button>
-            <button class="delete">Delete</button>
+            <button onclick = "deleteItem(${element.id})" class="delete">Delete</button>
           </li>
-          `;
+    `;
   });
+}
+function deleteItem(nazar) {
+  array = array.filter((item) => item.id !== nazar);
+  showUI(array);
 }
